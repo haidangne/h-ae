8bd6dd533c17e05f3d197656fc09d5295f64d5bf
haidangne <86764861+haidangne@users.noreply.github.com>
2021-07-01 13:39:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    introTitle: 'Hí Mõm Chó!',
+    introTitle: 'Hí anh em!',
     introDesc: `Trái đất vốn lạ thường
-   Thế đéo nào lại gặp được thằng mồm chó như mày`,
-    btnIntro: 'hihi',
+   Thế đéo nào lại gặp được 1 ng như Đăng`,
+    btnIntro: 'hí hí',
     title: 'Trả lời vài câu khảo sát chơi 😙',
-    desc: 'Hải Đăng là con chó? ',
-    btnYes: 'Chó mà còn hay sủa dơ :(( ',
-    btnNo: 'Chó cc à?',
+    desc: 'Hải Đăng là bồ ai ',
+    btnYes: 'bede ',
+    btnNo: 'bồ eim :))',
     question:'Nhận xét về Hải Đăng? ',
     btnReply: 'Trả Lời',
-    reply: 'Mồm thì thúi, hay đạo lý mà có gái là vểnh cặc bỏ ae, nhưng được cái giỏi xạo lồn hihi',
+    reply: 'thông minh , đáng yêu, vẽ đẹp, tư duy đỉnh cao là những gì Hải Đăng có :))',
     mess: 'Tui biết ae sẽ trả lời vậy mà :((',
-    messDesc: 'Đen từ trên da cho đến tâm hồn 😞',
-    btnAccept: 'Đăng Mõm Chó 😩',
+    messDesc: 'Toàn diện :V :V :V ',
+    btnAccept: 'Hải Đăng nè ',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
